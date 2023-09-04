793e8d0faa01ee337c3bf77c309339ea5b49a6dc
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 20:30:34 -0600

    Add files via upload

---
+++ 012_del.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+del colores[1]
+del colores[4]
+del colores[-4]
+del colores[-3]
+
+print (colores)
