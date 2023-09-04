2927bd4e6685bb89fad3e6a58c000e4cf6850038
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 20:54:04 -0600

    Add files via upload

---
+++ 014_eliminar_con_pop.py
@@ -0,0 +1,4 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+almacena1 = colores.pop(1)
+almacena2 = colores.pop(-2)
+print('El color almacenado es' + almacena1 + ' y ' + almacena2)
