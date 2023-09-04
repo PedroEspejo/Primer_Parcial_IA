108884d3a92654fcd48499cd8530174d2f2d7bc0
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 20:18:58 -0600

    Add files via upload

---
+++ 010_listas.py
@@ -0,0 +1,6 @@
+lista1 = ['uno','dos','tres','4',5]
+print(lista1[1])
+
+#EJERCICIO------------------------
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print(colores[3])
