37d6d7c9b92848c032b214b2108324dbb609e84c
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 19:24:35 -0600

    Add files via upload

---
+++ 005_metodos.py
@@ -0,0 +1,29 @@
+#TITLE------------------------------
+nombre = "pedro espejo vargas"
+nombre = nombre.title()
+print(nombre)
+
+#Tambien se puede como:
+nombre2 = "pedro espejo".title()
+print(nombre2)
+
+#UPPER------------------------------
+nombre3 = "pedro espejo".upper()
+print(nombre3)
+
+#LOWER----------------------------
+nombre4 = "PEDRO ESPEJO".lower()
+print(nombre4)
+
+#prueba
+prueba = "PEDRO ESPEJO".title()
+print(prueba)
+
+#ejercicio-----------------------
+ej1 = "enrique barros fernández".title()
+ej2 = "Esta Es Una Frase Para Ser Formateada.".lower()
+ej3 = "Esta Es Una Frase Para Ser Formateada.".upper()
+
+print(ej1)
+print(ej2)
+print(ej3)
---
+++ 006_salto_tab.py
@@ -0,0 +1,4 @@
+print("lenguajes\n\t-Python\n\t-Java\n\t-PHP\n\t-SQL\n\t-COBOL")
+
+
+
