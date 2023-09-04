4b3b42967db1dc9396727b58afdafffa734ad20d
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 21:52:38 -0600

    Add files via upload

---
+++ 021_if.py
@@ -0,0 +1,22 @@
+#36---------------------------------
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('Se ejecuta el if.')
+	
+
+#37--------------------------------
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if numero 2')
+	
+
+#38-----------------------------------
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if numero tres')
