b4a60733d9ff361ca3a91148c183d795eb10841d
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 19:09:27 -0600

    Add files via upload

---
+++ 004_concatenar.py
@@ -0,0 +1,15 @@
+nombre = 'Pedro'
+apellido = 'Espejo'
+
+nom_comp = nombre + ' ' + apellido
+print (nom_comp)
+
+num1 = 5
+num2 = 10
+num3 = num1 + num2
+print (num3)
+
+num4 = "5"
+num5 = "10"
+num6 = num4+num5
+print (num6)
