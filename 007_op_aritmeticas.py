a4dd9a5fc75dd931f65ffbb9afd51d4a5bade8a9
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-03 20:13:44 -0600

    Add files via upload

---
+++ 007_op_aritmeticas.py
@@ -0,0 +1,18 @@
+num1 = 20 
+num2 = 23
+num3 = 44
+
+sum = num1+num2+num3
+res = 0-num1-num2-num3
+
+mult = num1*num2
+
+div = 5000/230/2
+
+operacion = 10/5+15-17
+
+print(sum)
+print(res)
+print(mult)
+print(div)
+print(operacion)
---
+++ 008_exponentes.py
@@ -0,0 +1,5 @@
+exp=2**10
+normal = 2*2*2*2*2*2*2*2*2*2
+
+print(exp)
+print(normal)
