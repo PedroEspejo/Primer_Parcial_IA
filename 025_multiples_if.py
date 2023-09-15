d59b3d485b468c720dff90bf3f9a2396b7000ff9
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-15 12:48:31 -0600

    Add files via upload

---
+++ 025_multiples_if.py
@@ -0,0 +1,13 @@
+entrada = int(input('Introduce un numero del a al 4 \n'))
+
+if entrada == 1:
+    print('Opcion #1')
+
+if entrada == 2:
+    print('Opcion #2')
+
+if entrada == 3:
+    print('Opcion #3')
+
+if entrada == 4:
+    print('Opcion #4')
