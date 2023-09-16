92db6452270d41bfd320c5bd417ce39b19673f1f
PedroEspejo <143666358+PedroEspejo@users.noreply.github.com>
2023-09-15 21:12:51 -0600

    Add files via upload

---
+++ 031_diccionario.py
@@ -0,0 +1,15 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+consulta = teclado2['Modelo']
+consulta2 = teclado2['Precio']
+print(consulta, consulta2)
